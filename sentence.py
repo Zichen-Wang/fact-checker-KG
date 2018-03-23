fc764e8e23ce2d1399ae3c663731e081d8062441
Zichen Wang <wzc1995@pku.edu.cn>
2018-03-23 21:57:56 +0800

    fix indent

--- sentence.py
+++ sentence.py
@@ -1,14 +1,14 @@
 class Sentence():
-	def __init__(self, content, pos_tag, pos_in_text, number):
-		self.content = content
-		self.pos_tag = pos_tag
-		self.pos_in_text = pos_in_text
-		self.number = number
-	def get_context():
-		pass
-	def coreference_resolution():
-		pass
-	def extract_subject():
-		pass
-	def extract_predicate():
-		pass
+    def __init__(self, content, pos_tag, pos_in_text, number):
+        self.content = content
+        self.pos_tag = pos_tag
+        self.pos_in_text = pos_in_text
+        self.number = number
+    def get_context():
+        pass
+    def coreference_resolution():
+        pass
+    def extract_subject():
+        pass
+    def extract_predicate():
+        pass
