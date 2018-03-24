83bdfab5f42e4c4d48bb8b7c0e8a7d3c451d1eba
litian96 <tian.li@pku.edu.cn>
2018-03-24 15:44:59 +0800

    coreference resolution done, specification changed a little

--- src/sentence.py
+++ src/sentence.py
@@ -1,14 +1,74 @@
+# -*- coding: utf-8 -*-
+import nltk
+
+from neuralcoref import Coref
+
+#from predicate import Predicate
+#from text import Text
+
 class Sentence():
     def __init__(self, content, pos_tag, pos_in_text, number):
         self.content = content
         self.pos_tag = pos_tag
         self.pos_in_text = pos_in_text
         self.number = number
-    def get_context():
-        pass
-    def coreference_resolution():
-        pass
+        self.context = []
+        self.content_resolved = None
+        self.subject = None  # the raw subject in the sentence
+        self.predicate = None # the raw predicate verb/phrase in the sentence
+
+    def get_context(self, clean_text_content):
+
+        '''
+        for now, the context is just all sentences appearing beforehead
+        '''
+        all_sent = nltk.sent_tokenize(clean_text_content)
+        string_context=''
+        for i in all_sent[:self.pos_in_text]:
+            string_context += i
+        self.context = string_context
+
+        # print("[INFO]context for sentence " + self.content + " is: \n" + self.context)
+
+
+    def coreference_resolution(self):
+
+        '''
+        output: a string: the resolved content of this sentence
+        '''
+        
+        coref = Coref()
+        clusters = coref.one_shot_coref(utterances = unicode(self.content, "utf-8"), context = unicode(self.context, "utf-8"))
+
+        '''
+        @ problem here: the resolution result is not accurate enough, especially when the sentences are complicated!
+        '''
+        resolved_utterance_text = coref.get_resolved_utterances()
+        
+        self.content_resolved=str(resolved_utterance_text)
+        print(self.content_resolved)
+
     def extract_subject():
         pass
     def extract_predicate():
         pass
+
+
+if __name__ == "__main__":
+
+    '''
+    example paragraph:
+
+    Kepler orbits the Sun at a distance of 1.4-3.9 AU once every 4 years and 5 months (1,601 days). 
+    Its orbit has an eccentricity of 0.47 and an inclination of 15 with respect to the ecliptic. 
+    The body's observation arc begins at Heidelberg, the night after its official discovery observation.
+
+    '''
+
+    s = 'Kepler orbits the Sun once every 4 years and 5 months (1,601 days). Its orbit has an eccentricity of 0.47 and an inclination of 15° with respect to the ecliptic. The body\'s observation arc begins at Heidelberg, the night after its official discovery observation.'
+    text = Text(s, '1134 Kepler')
+
+    sentences = text.sentence_extractor()
+
+    for sentence in sentences:
+        sentence.get_context()
---
+++ src/test.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+from neuralcoref import Coref
+import nltk
+
+from text import Text
+#import nltk
+#
+#
+#sentence = 'Kepler orbits the Sun at a distance of 1.4-3.9 AU once every 4 years and 5 months (1,601 days). Its orbit has an eccentricity of 0.47 and an inclination of 15 with respect to the ecliptic.'
+#
+#tokens = nltk.word_tokenize(sentence)
+#tagged = nltk.pos_tag(tokens)
+#
+#namedEnt = nltk.ne_chunk(tagged)
+#
+#ne_in_sent = []
+#
+#
+#
+#for item in namedEnt.subtrees():
+#	if (item.label() == 'ORGANIZATION'):
+#		print(item.label())
+#
+#	print()
+#	print()
+#	#if subtree == 'Tree': 
+#	#	ne_label = subtree.label()
+#	#	ne_string = " ".join([token for token, pos in subtree.leaves()])
+#	#	ne_in_sent.append((ne_string, ne_label))
+
+
+if __name__ == "__main__":
+    '''
+    example paragraph:
+
+    Kepler orbits the Sun at a distance of 1.4-3.9 AU once every 4 years and 5 months (1,601 days). 
+    Its orbit has an eccentricity of 0.47 and an inclination of 15 with respect to the ecliptic. 
+    The body's observation arc begins at Heidelberg, the night after its official discovery observation.
+
+    '''
+    s = 'Kepler orbits the Sun once every 4 years and 5 months (1,601 days). Its orbit has an eccentricity of 0.47 and an inclination of 15° with respect to the ecliptic. The body\'s observation arc begins at Heidelberg, the night after its official discovery observation.'
+    text = Text(s, '1134 Kepler')
+
+    text.preprocessing()
+    sentences = text.sentence_extractor()
+
+    for sentence in sentences:
+              
+        sentence.get_context(text.clean_content)
+        sentence.coreference_resolution()
